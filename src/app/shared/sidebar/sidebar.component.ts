68ba83916cb780846a46525f1c71ec1a80c76a9c
John Nelson Florez <john.florez@mercadolibre.com.co>
2023-04-23 18:30:41 -0500

    Added sidebar without collapsed menu

--- src/app/shared/sidebar/sidebar.component.ts
+++ src/app/shared/sidebar/sidebar.component.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, Input, Output,EventEmitter } from '@angular/core'
 
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
   styleUrls: ['./sidebar.component.scss']
 })
+
+
+
 export class SidebarComponent {
 
+  isExpanded = true;
 }
